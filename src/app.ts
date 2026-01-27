279973fb3393ed4069d976e6ad57f8b5d3058d19
knaeeim <mdkhairulb01@gmail.com>
2026-01-28 01:49:45 +0600

    Better Auth Setup Done and Email Pass Auth is working fine

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express';
-import cors from 'cors'; 
+import cors from 'cors';
+import { auth } from './lib/auth';
+import { toNodeHandler } from "better-auth/node"
 
 const app: Application = express();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(cors({
-    origin : process.env.APP_URL, 
-    credentials : true
+    origin: process.env.APP_URL,
+    credentials: true
 }))
 
 
